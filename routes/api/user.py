235dffa406cceab9e01eeb2143d06d5a5b4b9472
OrTab <or.1532.tabin@gmail.com>
2024-07-14 01:19:14 +0300

    implement login logic

--- routes/api/user.py
+++ routes/api/user.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, redirect, flash
+from flask import Blueprint, request, redirect, flash, render_template
 from services.db_service import (
     db_operation,
     DatabaseOperationError,
@@ -14,9 +14,36 @@ def authenticate_user():
     return None
 
 
-@bp.route("/<user_id>")
-def get_user(user_id):
-    return {"answer": user_id}
+@bp.route("/login", methods=["POST"])
+def login():
+    try:
+        form_data = request.form
+        username = form_data.get("username")
+        password = form_data.get("password")
+        if not username or not password:
+            raise ValueError("Username or password cannot be empty.")
+
+        query = "SELECT * FROM users WHERE username = %s"
+        response = db_operation(query, (username,))
+        user = response[0]
+        if not user:
+            raise ValueError("Incorrect username or password.")
+
+        decrypted_password = encrypt_decrypt_password(user["password"])
+        if decrypted_password != password:
+            raise ValueError("Incorrect username or password.")
+
+        flash(f"Hey {username}, welcome back", category="message")
+        return redirect("/books")
+
+    except ValueError as error:
+        flash(str(error), category="error")
+        return render_template("login.html")
+
+    except Exception as error:
+        flash("An error occurred. Please try again later.", category="error")
+        print(f"Error during login: {error}")
+        return render_template("login.html")
 
 
 @bp.route("/signup", methods=["POST"])
@@ -29,10 +56,10 @@ def create_user():
     try:
         query = "INSERT INTO users (id, username, password, created_at) VALUES (UUID(), %s, %s, CURRENT_TIMESTAMP)"
         db_operation(query, user_data)
-        flash("user created successfully")
+        flash("user created successfully", category="success")
         return redirect("/signup")
     except DatabaseDuplicationEntryError:
-        flash("Username or email already exists. Please login.")
+        flash("Username or email already exists. Please login.", category="message")
         return redirect("/login")
     except DatabaseOperationError as error:
         if error.mysql_error_type == "duplicate_entry":
