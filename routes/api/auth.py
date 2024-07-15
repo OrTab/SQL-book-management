e7ffed40b873624f9b459948e2533c07661673de
OrTab <or.1532.tabin@gmail.com>
2024-07-16 00:30:03 +0300

    restructure login sign up endpoints

--- routes/api/auth.py
+++ routes/api/auth.py
@@ -1,3 +1,74 @@
 from flask import Blueprint
 
 bp = Blueprint("api", __name__)
+
+from flask import Blueprint, request, redirect, flash, render_template, url_for
+from services.db_service import (
+    db_operation,
+    DatabaseOperationError,
+    DatabaseDuplicationEntryError,
+)
+from services.users_service import (
+    encrypt_decrypt_password,
+    validate_username_password_existence,
+)
+
+
+@bp.before_request
+def authenticate_user():
+    return None
+
+
+@bp.route("/login", methods=["POST"])
+def login():
+    try:
+        form_data = request.form
+        username = form_data.get("username")
+        password = form_data.get("password")
+        validate_username_password_existence(username, password)
+
+        query = "SELECT * FROM users WHERE username = %s"
+        response = db_operation(query, (username,))
+        user = response[0] if response else None
+        if not user:
+            raise ValueError("Incorrect username or password.")
+
+        decrypted_password = encrypt_decrypt_password(user["password"])
+        if decrypted_password != password:
+            raise ValueError("Incorrect username or password.")
+
+        flash(f"Hey {username}, welcome back", category="message")
+        return redirect("/books")
+
+    except ValueError as error:
+        flash(str(error), category="error")
+        return render_template("login.html", username=username)
+    except Exception as error:
+        flash("An error occurred. Please try again later.", category="error")
+        print(f"Error during login: {error}")
+        return render_template("login.html")
+
+
+@bp.route("/signup", methods=["POST"])
+def create_user():
+    try:
+        form_data = request.form
+        username = form_data.get("username")
+        password = form_data.get("password")
+        validate_username_password_existence(username, password)
+        password = encrypt_decrypt_password(password)
+        user_data = (username, password)
+        query = "INSERT INTO users (id, username, password, created_at) VALUES (UUID(), %s, %s, CURRENT_TIMESTAMP)"
+        db_operation(query, user_data)
+        flash("user created successfully", category="success")
+        flash(f"Hey {username}, welcome back", category="message")
+        return redirect("/books")
+    except ValueError as error:
+        flash(str(error), category="error")
+        return render_template("signup.html")
+    except DatabaseDuplicationEntryError:
+        flash("Username or email already exists. Please login.", category="message")
+        return redirect(url_for("index.login", username=username))
+    except DatabaseOperationError as error:
+        flash("An unexpected error occurred. Please try again later.", category="error")
+        return render_template("signup.html")
--- routes/api/user.py
+++ routes/api/user.py
@@ -1,24 +1,14 @@
-from flask import Blueprint, request, redirect, flash, render_template, url_for
+from flask import Blueprint, request
 from services.db_service import (
     db_operation,
     DatabaseOperationError,
-    DatabaseDuplicationEntryError,
-)
-from services.users_service import (
-    encrypt_decrypt_password,
-    validate_username_password_existence,
 )
 
 bp = Blueprint("user", __name__)
 
 
-@bp.before_request
-def authenticate_user():
-    return None
-
-
 @bp.route("/load_test")
-def load_test():
+def load_test_find_user():
     try:
         username = request.args.get("username")
         query = "SELECT * FROM users WHERE username = %s"
@@ -30,58 +20,3 @@ def load_test():
             return "not found"
     except DatabaseOperationError as error:
         return error.message, error.status_code
-
-
-@bp.route("/login", methods=["POST"])
-def login():
-    try:
-        form_data = request.form
-        username = form_data.get("username")
-        password = form_data.get("password")
-        validate_username_password_existence(username, password)
-
-        query = "SELECT * FROM users WHERE username = %s"
-        response = db_operation(query, (username,))
-        user = response[0] if response else None
-        if not user:
-            raise ValueError("Incorrect username or password.")
-
-        decrypted_password = encrypt_decrypt_password(user["password"])
-        if decrypted_password != password:
-            raise ValueError("Incorrect username or password.")
-
-        flash(f"Hey {username}, welcome back", category="message")
-        return redirect("/books")
-
-    except ValueError as error:
-        flash(str(error), category="error")
-        return render_template("login.html", username=username)
-    except Exception as error:
-        flash("An error occurred. Please try again later.", category="error")
-        print(f"Error during login: {error}")
-        return render_template("login.html")
-
-
-@bp.route("/signup", methods=["POST"])
-def create_user():
-    try:
-        form_data = request.form
-        username = form_data.get("username")
-        password = form_data.get("password")
-        validate_username_password_existence(username, password)
-        password = encrypt_decrypt_password(password)
-        user_data = (username, password)
-        query = "INSERT INTO users (id, username, password, created_at) VALUES (UUID(), %s, %s, CURRENT_TIMESTAMP)"
-        db_operation(query, user_data)
-        flash("user created successfully", category="success")
-        flash(f"Hey {username}, welcome back", category="message")
-        return redirect("/books")
-    except ValueError as error:
-        flash(str(error), category="error")
-        return render_template("signup.html")
-    except DatabaseDuplicationEntryError:
-        flash("Username or email already exists. Please login.", category="message")
-        return redirect(url_for("index.login", username=username))
-    except DatabaseOperationError as error:
-        flash("An unexpected error occurred. Please try again later.", category="error")
-        return render_template("signup.html")
